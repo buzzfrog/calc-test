da0723b25e2e0d1c7c4801b10cdf9ffd4e5dd28e
Dag König <dagk@microsoft.com>
2018-12-18 13:22:06 +0100

    add implemented

---
+++ src/Calc.WebAPI.Tests/CalcControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Calc.WebAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Calc.WebAPI.Tests
+{
+    public class CalcControllerTests
+    {
+        [Fact]
+        public void Add_5_6_should_return_11()
+        {
+            var calcController = new CalcController();
+
+            var result = (ObjectResult) calcController.Add(new values() { value1 = "5", value2 = "6" });
+
+            Assert.Equal(11, result.Value);
+        }
+
+        [Fact]
+        public void Add_one_invalid_number_should_return_error()
+        {
+            var calcController = new CalcController();
+
+            var result = (ObjectResult)calcController.Add(new values() { value1 = "5", value2 = "6a" });
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+    }
+}
---
+++ src/Calc.WebAPI/Controllers/CalcController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calc.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+
+        [Route("add")]
+        [HttpPost]
+        public IActionResult Add([FromBody] values value)
+        {
+            try
+            {
+                int v1 = Convert.ToInt32(value.value1);
+                int v2 = Convert.ToInt32(value.value2);
+
+                return new ObjectResult(v1+v2);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest("not valid numbers");
+            }
+        }
+    }
+
+    public class values
+    {
+        public string value1 { get; set; }
+        public string value2 { get; set; }
+    }
+}
